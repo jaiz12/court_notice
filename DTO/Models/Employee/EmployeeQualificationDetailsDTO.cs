d6f9ca203fd8d4ca5270d166d9b4b384d95334da
Pranai Giri <pranaigirisikkim@gmail.com>
2024-01-03 09:53:41 +0530

    Qualification Update

--- DTO/Models/Employee/EmployeeQualificationDetailsDTO.cs
+++ DTO/Models/Employee/EmployeeQualificationDetailsDTO.cs
@@ -14,9 +14,9 @@ namespace DTO.Models.Employee
         public int year_of_passing { get; set; }
         public long? board_id { get; set; }
         public long? specialization_id { get; set; }
-        public long stream_id { get; set; }
+        public long? stream_id { get; set; }
         public string grade { get; set; }
-        public decimal percentage { get; set; }
+        public decimal? percentage { get; set; }
         public string? attachment_url { get; set; }
     }
 }
