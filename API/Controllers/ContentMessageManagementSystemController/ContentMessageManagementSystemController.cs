c9c1ae9ee97786e6564bab62d374197c643c937b
mukesh.shah <mukesh.shah@SBNSLGLAPIT004>
2024-01-05 17:27:04 +0530

    created new api for cms message management

--- API/Controllers/ContentMessageManagementSystemController/ContentMessageManagementSystemController.cs
+++ API/Controllers/ContentMessageManagementSystemController/ContentMessageManagementSystemController.cs
@@ -1,13 +1,14 @@
 ﻿using API.Services;
-using BAL.Services.ContentManagementSystem.ContentManagementSystem;
+using BAL.Services.ContentManagementSystem.ContentMessageManagementSystem;
 using DTO.Models.Auth;
 using DTO.Models.ContentManagementSystem;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 using System.Threading.Tasks;
 
-namespace API.Controllers.ContentManagementSystemController
+namespace API.Controllers.ContentMessageManagementSystemController
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -15,26 +16,27 @@ namespace API.Controllers.ContentManagementSystemController
 
     // =============================================
     // -- Author:		Mukesh Shah
-    // -- Create date:  07-Dec-2023
+    // -- Create date:  05-Jan-2024
     // =============================================
-    public class ContentManagementSystemController : ControllerBase
+    public class ContentMessageManagementSystemController : ControllerBase
     {
-        private readonly IContentManagementSystemService _contentManagementSystemService;
+        private readonly IContentMessageManagementSystemService _contentMessageManagementSystemService;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IAuthoriseRoles _authoriseRoles;
-        public ContentManagementSystemController(IContentManagementSystemService contentManagementSystemService, 
+
+        public ContentMessageManagementSystemController(IContentMessageManagementSystemService contentMessageManagementSystemService, 
             UserManager<ApplicationUser> userManager,
-            RoleManager<IdentityRole> roleManager, 
+            RoleManager<IdentityRole> roleManager,
             IAuthoriseRoles authoriseRoles)
         {
-            _contentManagementSystemService = contentManagementSystemService;
-            _authoriseRoles = authoriseRoles;
+            _contentMessageManagementSystemService = contentMessageManagementSystemService;
             _userManager = userManager;
             _roleManager = roleManager;
+            _authoriseRoles = authoriseRoles;
         }
-        [HttpPost("PostCMSDetails/{userId}/{companyName}")]
-        public async Task<IActionResult> PostCMSDetails(ContentManagementSystem_DTO cmsDetails, string userId, string companyName)
+        [HttpPost("PostCmsMessage/{userId}/{companyName}")]
+        public async Task<IActionResult> PostCmsMessage(ContentMessageManagementSystem_DTO message, string userId, string companyName)
         {
             var userCompanyRoleValidate = await _authoriseRoles.AuthorizeUserRole(userId, companyName, "'Company Head','Super Admin'", _roleManager, _userManager);
             if (!userCompanyRoleValidate)
@@ -43,16 +45,16 @@ namespace API.Controllers.ContentManagementSystemController
             }
             try
             {
-                var result = await _contentManagementSystemService.PostCMSDetails(cmsDetails);
+                var result = await _contentMessageManagementSystemService.PostCmsMessage(message);
                 return Ok(new { result = result });
             }
             catch
             {
-                return  BadRequest();
+                return BadRequest();
             }
         }
-        [HttpPut("PutCMSDetails/{userId}/{companyName}")]
-        public async Task<IActionResult> PutCMSDetails(ContentManagementSystem_DTO cmsDetails, string userId, string companyName)
+        [HttpPut("PutCmsMessage/{userId}/{companyName}")]
+        public async Task<IActionResult> PutCmsMessage(ContentMessageManagementSystem_DTO message, string userId, string companyName)
         {
             var userCompanyRoleValidate = await _authoriseRoles.AuthorizeUserRole(userId, companyName, "'Company Head','Super Admin'", _roleManager, _userManager);
             if (!userCompanyRoleValidate)
@@ -61,7 +63,7 @@ namespace API.Controllers.ContentManagementSystemController
             }
             try
             {
-                var result = await _contentManagementSystemService.PutCMSDetails(cmsDetails);
+                var result = await _contentMessageManagementSystemService.PutCmsMessage(message);
                 return Ok(new { result = result });
             }
             catch
@@ -69,8 +71,8 @@ namespace API.Controllers.ContentManagementSystemController
                 return BadRequest();
             }
         }
-        [HttpDelete("DeleteCMSDetails/{userId}/{companyName}/{cms_id}")]
-        public async Task<IActionResult> DeleteCMSDetails(long cms_id, string userId, string companyName)
+        [HttpDelete("DeleteCmsMessage/{userId}/{companyName}/{message_id}")]
+        public async Task<IActionResult> DeleteCmsMessage(long message_id, string userId, string companyName)
         {
             var userCompanyRoleValidate = await _authoriseRoles.AuthorizeUserRole(userId, companyName, "'Company Head','Super Admin'", _roleManager, _userManager);
             if (!userCompanyRoleValidate)
@@ -79,7 +81,7 @@ namespace API.Controllers.ContentManagementSystemController
             }
             try
             {
-                var result = await _contentManagementSystemService.DeleteCMSDetails(cms_id);
+                var result = await _contentMessageManagementSystemService.DeleteCmsMessage(message_id);
                 return Ok(new { result = result });
             }
             catch
@@ -87,35 +89,17 @@ namespace API.Controllers.ContentManagementSystemController
                 return BadRequest();
             }
         }
-        [HttpGet("GetCMSDetails/{userId}/{companyName}")]
-        public async Task<IActionResult> GetCMSDetails(string userId, string companyName)
+        [HttpGet("GetCmsMessage/{userId}/{companyName}")]
+        public async Task<IActionResult> GetCmsMessage(string userId, string companyName)
         {
-            var userCompanyRoleValidate = await _authoriseRoles.AuthorizeUserRole(userId, companyName, "'Company Head','Super Admin','Employee','Manager','Admin'", _roleManager, _userManager);
+            var userCompanyRoleValidate = await _authoriseRoles.AuthorizeUserRole(userId, companyName, "'Company Head','Super Admin'", _roleManager, _userManager);
             if (!userCompanyRoleValidate)
             {
                 return BadRequest(new { message = "Unauthorize User.", messageDescription = "You are not authorize to use the module. Please contact with your admin for the permission" });
             }
             try
             {
-                var result = await _contentManagementSystemService.GetCMSDetails();
-                return Ok(result);
-            }
-            catch
-            {
-                return BadRequest();
-            }
-        }
-        [HttpGet("GetCMSDetailsOnLoad/{userId}/{companyName}")]
-        public async Task<IActionResult> GetCMSDetailsOnLoad(string userId, string companyName)
-        {
-            var userCompanyRoleValidate = await _authoriseRoles.AuthorizeUserRole(userId, companyName, "'Admin','Super Admin', 'Company Head', 'Employee', 'Manager'", _roleManager, _userManager);
-            if (!userCompanyRoleValidate)
-            {
-                return BadRequest(new { message = "Unauthorize User.", messageDescription = "You are not authorize to use the module. Please contact with your admin for the permission" });
-            }
-            try
-            {
-                var result = await _contentManagementSystemService.GetCMSDetailsOnLoad();
+                var result = await _contentMessageManagementSystemService.GetCmsMessage();
                 return Ok(result);
             }
             catch
