c393315ca73c906d1b7c7cbf0821da0136249abd
jaideep.roy <jaideep.roy@henoistech.com>
2024-01-17 10:04:45 +0530

    add roles

--- API/Controllers/ContentManagementSystemController/ContentManagementSystemController.cs
+++ API/Controllers/ContentManagementSystemController/ContentManagementSystemController.cs
@@ -90,7 +90,7 @@ namespace API.Controllers.ContentManagementSystemController
         [HttpGet("GetCMSDetails/{userId}/{companyName}")]
         public async Task<IActionResult> GetCMSDetails(string userId, string companyName)
         {
-            var userCompanyRoleValidate = await _authoriseRoles.AuthorizeUserRole(userId, companyName, "'Company Head','Super Admin','Employee','Manager','Admin'", _roleManager, _userManager);
+            var userCompanyRoleValidate = await _authoriseRoles.AuthorizeUserRole(userId, companyName, "'Company Head','Super Admin','Leave Admin','Employee','Manager','Admin'", _roleManager, _userManager);
             if (!userCompanyRoleValidate)
             {
                 return BadRequest(new { message = "Unauthorize User.", messageDescription = "You are not authorize to use the module. Please contact with your admin for the permission" });
@@ -108,7 +108,7 @@ namespace API.Controllers.ContentManagementSystemController
         [HttpGet("GetCMSDetailsOnLoad/{userId}/{companyName}")]
         public async Task<IActionResult> GetCMSDetailsOnLoad(string userId, string companyName)
         {
-            var userCompanyRoleValidate = await _authoriseRoles.AuthorizeUserRole(userId, companyName, "'Admin','Super Admin', 'Company Head', 'Employee', 'Manager'", _roleManager, _userManager);
+            var userCompanyRoleValidate = await _authoriseRoles.AuthorizeUserRole(userId, companyName, "'Admin','Super Admin','Leave Admin', 'Company Head', 'Employee', 'Manager'", _roleManager, _userManager);
             if (!userCompanyRoleValidate)
             {
                 return BadRequest(new { message = "Unauthorize User.", messageDescription = "You are not authorize to use the module. Please contact with your admin for the permission" });
