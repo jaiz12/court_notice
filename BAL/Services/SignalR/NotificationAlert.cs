212d367b4f082816c5b0b022c735b9443e0104c8
Pranai Giri <pranaigirisikkim@gmail.com>
2024-02-01 10:37:45 +0530

    Message and Notice is now Realtime

---
+++ BAL/Services/SignalR/NotificationAlert.cs
@@ -0,0 +1,29 @@
+﻿using DTO.Models.Employee;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Services.SignalR
+{
+    public class NotificationAlert : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            try
+            {
+                await Clients.Caller.SendAsync("NewNoticeAlert");
+                await Clients.Caller.SendAsync("NewMessageAlert");
+                await base.OnConnectedAsync();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                Console.WriteLine($"Error in OnConnectedAsync method: {ex.Message}");
+                throw new Exception(ex.Message); // Rethrow the exception to signal the error
+            }
+        }
+    }
+}
